42cefd8e16d5116116b3a37bbfb458da28d361e6
Shierley NAU <shierleyarlita@gmail.com>
2026-04-03 15:30:35 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    remotePatterns:[
+  images: {
+    remotePatterns: [
       {
-        protocol :"https",
-        hostname :"be-sporton.agunacourse.com",
-        pathname :"/uploads/**",
-      }
-    ]
-  }
+        protocol: "http",
+        hostname: "localhost",
+        port: "5001",
+        pathname: "/uploads/**",
+      },
+    ],
+    dangerouslyAllowLocalIP: true,
+  },
 };
 
 export default nextConfig;
